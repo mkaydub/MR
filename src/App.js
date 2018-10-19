136725755924e98c2fba9663009251be7a5429da
Melissa Key West <art@mkaydub.com>
2018-10-19 12:16:58 -0600

    Refactored repetitive code from Home & Search comps into App.js to avoid roundtrip to the server on each page load.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,54 @@ import './App.css'
 import { Route } from 'react-router-dom'
 import Home from './views/Home.js'
 import Search from './views/Search.js'
+import * as BooksAPI from './BooksAPI.js'
 
 
 class BooksApp extends React.Component {
+	state = {
+		books: [],
+	}
+
+	componentDidMount = () => {
+		this.mountAllBooks();
+	}
+
+	mountAllBooks = () => {
+		BooksAPI.getAll()
+			.then( allBooks => {
+				this.setState( {
+					books: allBooks
+				} );
+			} );
+	}
+
+
+	updateShelf = ( book, shelf ) => {
+		BooksAPI.update( book, shelf )
+			.then( allBooks => {
+				book.shelf = shelf;
+				this.setState( state => ( {
+					books: this.state.books.filter( b => b.id !== book.id ).concat( [
+						book
+					] )
+				} ) )
+			} );
+	}
 
 	render() {
 		return (
 			<div>
-        <Route exact path ="/" component= { Home } />
-        <Route exact path ="/search" component= { Search } />
+        <Route exact path ="/" render={(() => (<Home
+          books={this.state.books}
+          onUpdateShelf={this.updateShelf}
+          onMountAllBooks={this.mountAllBooks}/>))}
+					/>
+
+        <Route exact path ="/search" render={(() => (<Search
+						books={this.state.books}
+						onUpdateShelf={this.updateShelf}
+						onMountAllBooks={this.mountAllBooks} />))}
+						/>
       </div>
 		);
 	}
